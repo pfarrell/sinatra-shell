43b42f41a03f83a8350084fb95aa65da2c0daa37
Patrick Farrell <patrick.farrell@datarobot.com>
2020-05-16 12:33:09 -0700

    version updates and split db into separate file

--- app/models.rb
+++ app/models.rb
@@ -1,23 +1,3 @@
 require 'db'
-require 'sequel'
-require 'pg'
-require 'logger'
-
-$console = ENV['RACK_ENV'] == 'development' ? Logger.new(STDOUT) : nil
-DB = Sequel.connect(
-  ENV['APP_DB'] || 'postgres://localhost/app',
-  logger: $console,
-  test: true
-)
-
-DB.sql_log_level = :debug
-DB.extension(:pagination)
-DB.extension(:pg_array, :pg_json)
-DB.extension(:connection_validator)
-
-DB.pool.connection_validation_timeout = 300
-
-Sequel::Model.plugin :timestamps
-Sequel::Model.plugin :json_serializer
 
 #require models/* files
